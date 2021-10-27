b12906b11b5d5f45a707520499660cec92ab86fe
avi-lucky <[shuklaavikal7@gmail.com]>
2021-10-28 02:53:38 +0530

    Getting User Input & Handling Form Submission

--- src/components/layout/meetups/NewMeetupForm.js
+++ src/components/layout/meetups/NewMeetupForm.js
@@ -1,9 +1,30 @@
 import Card from "../ui/Card";
 import classes from "./NewMeetupForm.module.css";
 
+import { useRef } from 'react';
+
 function NewMeetupForm() {
-  function submitHandler(event) {
+    const titleInputRef = useRef();
+    const imageInputRef = useRef();
+    const addressInputRef = useRef();
+    const descriptionInputRef = useRef();
+
+  function submitHandler(event) {   
     event.preventDefault();
+
+    const enteredTitle = titleInputRef.current.value;
+    const enteredImage = imageInputRef.current.value;
+    const enteredAddress = addressInputRef.current.value;
+    const enteredDescription = descriptionInputRef.current.value;
+
+    const meetupData = {
+        title: enteredTitle,
+        image: enteredImage,
+        address: enteredAddress,
+        description: enteredDescription,
+    };
+
+    console.log(meetupData);
   }
 
   return (
@@ -11,7 +32,7 @@ function NewMeetupForm() {
       <form className={classes.form} onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor="title">Meetup Title</label>
-          <input type="text" required id="title" />
+          <input type="text" required id="title" ref={titleInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor="image">Meetup Image</label>
