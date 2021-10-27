dd778d9fe873b75584ce8cc8610ffafa40625ace
avi-lucky <[shuklaavikal7@gmail.com]>
2021-10-27 11:25:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Route } from 'react-router-dom';
+
+import AllMeetupsPage from './pages/AllMeetups';
+import FavoritesPage from './pages/Favorites';
+import NewMeetupPage from './pages/NewMeetup';
+
+function App() {
+  return <div>
+    <Route></Route>
+  </div>;
+}
+
+export default App;
