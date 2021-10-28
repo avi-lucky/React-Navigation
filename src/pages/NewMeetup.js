b81d7b49cc63cfa909c7089119b9a218bfe44638
avi-lucky <[shuklaavikal7@gmail.com]>
2021-10-28 14:16:13 +0530

    Data Getting Stored In Firebase

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,25 @@
-import NewMeetupForm from "../components/layout/meetups/NewMeetupForm";
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
-    return <section>
-        <h1>Add New Meetup</h1>
-        <NewMeetupForm /> 
-    </section>;
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-navigation-d6cdb-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
 }
 
 export default NewMeetupPage;
